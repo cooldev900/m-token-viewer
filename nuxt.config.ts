70f826c24af96850f9cfba85a42f8ec79cd240e6
cooldev900 <cooldev900@gmail.com>
2024-11-29 11:30:29 -0500

    env: add project id for web3modal

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      VITE_APP_PROJECT_ID: process.env.VITE_APP_PROJECT_ID,
+    },
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true }
 })
